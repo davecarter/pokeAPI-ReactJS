a82954f84be9b49d88c0f9222167cfa84204d129
David Garcia <david.garcia@scmspain.com>
2016-10-31 13:03:13 +0100

    get height and weight

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Card from '@schibstedspain/sui-card';
 import './index.scss';
 
-const pokemons = fetch('http://pokeapi.co/api/v2/pokemon/')
+const pokemons = fetch('http://pokeapi.co/api/v2/pokemon/?offset=5')
   .then(response => response.json())
   .then(data => data.results)
   .catch(error => { console.log('Request failed', error) })
@@ -12,27 +12,56 @@ class Pokewrapper extends Component {
   constructor(...args){
     super(...args)
     this.state = {
-      pokemons : []
+      pokemons: {}
     }
   }
 
   componentDidMount () {
     this.props.pokemonsPromise
-    .then( pokemons => this.setState({pokemons}))
+    .then( pokemons => {
+      const pokemonsObject = {}
+      pokemons.forEach(pokemon => {
+        const pokemonName = pokemon.name
+        pokemonsObject[pokemonName] = pokemon
+        this.setPokemonData(pokemonName)
+      })
+
+      this.setState({ pokemons: pokemonsObject })
+    } )
+  }
+
+  setPokemonData (pokemonName) {
+    fetch(`http://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then(response => response.json())
+      .then(data => {
+        // Create new pokemons object avoiding original reference to state
+        let { pokemons } = this.state
+        // Specific pokemon original data (name and url)
+        let pokemonData = pokemons[pokemonName]
+        // Merge original data with new one from the API reponse
+        pokemonData = Object.assign(pokemonData, data)
+        // Merge original pokemons object with new key from APi response data
+        pokemons = Object.assign(pokemons, { [pokemonName]: pokemonData })
+        // Finally save it to state
+        this.setState( { pokemons } )
+      })
   }
 
   render () {
-    const pokemonName = this.state.pokemons.map( (pokemon, index) => {
-      const url = `https://img.pokemondb.net/artwork/${pokemon.name}.jpg`
+    const { pokemons } = this.state
+    const pokemonCard = Object.keys(pokemons).map( (pokemon, index) => {
+      const pokemonData = pokemons[pokemon]
+      const url = `https://img.pokemondb.net/artwork/${pokemonData.name}.jpg`
       return (
         <Card key={index} className='pokeCard'
           primary={
-            <img title={pokemon.name} src={url} />
+            <img title={pokemonData.name} src={url} />
           }
           secondary={
             <div className='data'>
-              <h2 className='sui-Card-title'>{pokemon.name}</h2>
-              <p className='sui-Card-description'>{pokemon.weight}</p>
+              <h2 className='pokeCard-title'>{pokemonData.name}</h2>
+              {pokemonData.weight && <p>Weight: {pokemonData.weight}kg</p>}
+              {pokemonData.height && <p>Height: {pokemonData.height}</p>}
             </div>
           }
         />
@@ -41,7 +70,7 @@ class Pokewrapper extends Component {
 
     return (
         <div className='pokeWrapper'>
-          {pokemonName}
+          {pokemonCard}
         </div>
     )
   }
